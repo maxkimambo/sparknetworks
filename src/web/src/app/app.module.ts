e1c926db5246f067b8e7dcb726d6228d4fc56a9f
Max Kimambo <max@kimambo.de>
2018-04-18 16:15:25 +0200

    add http client module

--- src/web/src/app/app.module.ts
+++ src/web/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'; 
 
 import { FilterComponent } from './components/app.filter';
 import { HeaderComponent} from './components/app.header';
@@ -20,7 +21,8 @@ import {AppComponent} from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
